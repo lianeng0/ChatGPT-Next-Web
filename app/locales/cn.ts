485995c51c3563701f0fb112164fd8a1545f2891
lianeng0 <35573333+lianeng0@users.noreply.github.com>
2023-03-30 01:42:38 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -3,7 +3,7 @@ import { SubmitKey } from "../store/app";
 const cn = {
   WIP: "该功能仍在开发中……",
   Error: {
-    Unauthorized: "现在是未授权状态，请在设置页填写授权码。",
+    Unauthorized: "未授权使用，请联系微信： zen25885 获取授权码。",
   },
   ChatItem: {
     ChatItemCount: (count: number) => `${count} 条对话`,
